4d7f9bb4529d68f5305bf27771a526220352a9a0
Sarojsin <sarojsinghdhami91@gmail.com>
2026-03-14 11:20:52 +0545

    refactor: update import paths for database engine in migration scripts

--- scripts/migrations/migrate_profile_pic.py
+++ scripts/migrations/migrate_profile_pic.py
@@ -6,7 +6,7 @@ PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 if PROJECT_ROOT not in sys.path:
     sys.path.append(PROJECT_ROOT)
 
-from database.database import engine
+from app.core.database import engine
 from sqlalchemy import text
 
 
